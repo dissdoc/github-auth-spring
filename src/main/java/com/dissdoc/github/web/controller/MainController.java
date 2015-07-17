8e35155fbbc39e3cfde43568a5a5b9911811ea3d
Kirill Mukhov <dissdoc@gmail.com>
2015-07-17 18:06:43 +0300

    refactoring init project with xml config

---
+++ src/main/java/com/dissdoc/github/web/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.dissdoc.github.web.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by KMukhov on 16.07.2015.
+ */
+@RestController
+@RequestMapping("/welcome")
+public class MainController {
+
+    @RequestMapping(value = "")
+    public String printWelcome(ModelMap model) {
+        return "hello";
+    }
+}
